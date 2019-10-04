37fef4854670e1183b40f41bca7b4d09736682a5
Roman Piskun <roman.piskun@globallogic.com>
2019-10-04 11:23:05 +0300

    Add lesson4

--- lesson4/module_install.sh
+++ lesson4/module_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-DEST_PATH=lesson3
+DEST_PATH=lesson4
 mkdir $ROOTFS/lib/modules/$DEST_PATH
-cp hello.ko $ROOTFS/lib/modules/$DEST_PATH
+cp hrtimer.ko $ROOTFS/lib/modules/$DEST_PATH
 pushd .
 cd $ROOTFS
 find . | cpio -o -H newc | gzip > ../rootfs.cpio.gz
