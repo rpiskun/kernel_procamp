ead235f3dcb6b11ba374e38977ca44b4e07b3dcd
Roman Piskun <roman.piskun@globallogic.com>
2019-10-06 16:39:23 +0300

    Fix lesson4: do not use flags

--- lesson4/hrtimer.c
+++ lesson4/hrtimer.c
@@ -9,21 +9,24 @@ MODULE_AUTHOR("Roman Piskun");
 MODULE_DESCRIPTION("Test timers");
 MODULE_VERSION("0.22");
 
+static int count = 0;
+
 static void staticTimerExpiredCb(struct timer_list *pTmList);
 
-// DEFINE_TIMER(myStaticTim, staticTimerExpiredCb);
-static struct timer_list myStaticTim;
+static DEFINE_TIMER(myStaticTim, staticTimerExpiredCb);
 
 static void staticTimerExpiredCb(struct timer_list *pTmList)
 {
-    printk(KERN_INFO "Timer expired! flags: %d!\n", pTmList->flags);
+    printk(KERN_INFO "Timer expired!\n");
+    if (++count <= 5)
+    {
+        printk(KERN_INFO "Timer rescheduled; cnt: %d!\n", count);
+        mod_timer(&myStaticTim, jiffies + HZ * 2);
+    }
 }
 
 static int __init lesson4_init(void)
 {
-    // myStaticTim.flags = 5;
-    timer_setup(&myStaticTim, staticTimerExpiredCb, 5);
-
     myStaticTim.expires = jiffies + 2 * HZ;
     add_timer(&myStaticTim);
 
@@ -43,6 +46,8 @@ static void __exit lesson4_exit(void)
     }
 
     printk(KERN_INFO "timer deleted\n");
+
+    printk(KERN_INFO "module exit\n");
 }
 
 module_init(lesson4_init);
